8e5f0cfe1f25b16f6c4a5a5eea6d0dca6ca26a5b
trongan123 <62749635+trongan123@users.noreply.github.com>
2022-07-04 10:11:04 +0700

    add odata

--- BookingAPI/Program.cs
+++ BookingAPI/Program.cs
@@ -1,11 +1,19 @@
+using DataAccess.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
 using Repositories.IService;
 using Repositories.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddOData(option => option.Select().Filter()
+          .Count().OrderBy().Expand().SetMaxTop(106).AddRouteComponents("odata", GetEdmModel()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,6 +41,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+app.UseODataBatching();
 
 app.UseHttpsRedirection();
 
@@ -41,3 +57,23 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static IEdmModel GetEdmModel()
+{
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Account>("Accounts");
+    builder.EntitySet<Bill>("Bill");
+    builder.EntitySet<BookingRoomDetail>("BookingRoomDetails");
+    builder.EntitySet<BookingSeviceDetail>("BookingSeviceDetails");
+    builder.EntitySet<BookingTransportDetail>("BookingTransportDetails");
+    builder.EntitySet<Comment>("Comments");
+    builder.EntitySet<Room>("Rooms");
+    builder.EntitySet<RoomType>("RoomTypes");
+    builder.EntitySet<Service>("Services");
+    builder.EntitySet<Transport>("Transports");
+    builder.EntitySet<TypeTransport>("TypeTransports");
+
+
+    return builder.GetEdmModel();
+
+}
