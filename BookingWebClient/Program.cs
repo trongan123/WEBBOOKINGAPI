c7fd8f43da677ad3aac4fab978f2380fbc1b9e70
trongan123 <62749635+trongan123@users.noreply.github.com>
2022-07-04 16:13:22 +0700

    Revert "Revert "update login""
    This reverts commit 9921ff9325073c41b34b2ac6475c9052a359d80c.

--- BookingWebClient/Program.cs
+++ BookingWebClient/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -12,7 +13,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -22,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
